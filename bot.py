95c6f147d622a9fcc4013fc12aae0e2e515e2814
Ryan Sowden <Ryan@Susans-MacBook-Pro.local>
2021-07-16 16:53:44 +1000

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,52 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+from sleeper_wrapper import League
+from sleeper_wrapper import Players
+import pandas as pd
+from tabulate import tabulate
+
+load_dotenv() #loading the .env file
+TOKEN = os.getenv('DISCORD_TOKEN') #fetches the discord token from .env file
+league = () #setting the league variable
+
+client = commands.Bot(command_prefix=';')
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+#@client.command(name='assign', help='This lests the admin user assign the league ID from sleeper to a key word e.g Green')
+#async def assign_key_value(ctx,*args):
+ #   league_number = args
+#   league_name = args
+ #   new_league_name = league_name(args)
+  #  await ctx.send(new_leauge_name)
+
+#error handling & logging for easy tracking, *writes to err.log
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+client.run(TOKEN)
+
+
---
+++ cogs/standings.py
@@ -0,0 +1,39 @@
+import discord 
+from discord.ext import commands
+from sleeper_wrapper import League
+from sleeper_wrapper import Players
+import pandas as pd
+from tabulate import tabulate
+
+
+
+
+#bot command to get the standings for the requested league, will work for any sleeper league as logn as you have the league ID.
+
+class Standings(commands.Cog):
+
+    def __init__(self,client):
+        self.client = client
+
+
+    @commands.command(name='standings', help='This gets standings for the requested lea    gue')
+    async def get_standings(self,ctx,arg):
+        self.league = League(arg)
+        self.league_name = self.league.get_league()
+        self.rosters = self.league.get_rosters()
+        self.users = self.league.get_users()
+        self.standings = self.league.get_standings(self.rosters,self.users)
+        self.df = pd.DataFrame.from_dict(self.standings)
+        self.df.index = self.df.index + 1
+        self.results = (self.df[self.df.columns[[0,1,2]]])
+        self.results['W/L']=self.results[1] + '-'+ self.results[2]
+        self.table_data = self.results.drop([1,2], axis=1)
+        self.table = "```" + tabulate(self.table_data, headers=['Name','W/L'], showindex=False, tablefmt='plain') + "```"    
+        self.embed = discord.Embed(title=self.league_name['name'] + ' ' + 'Standings', description=self.table, colour=15158332)
+        await ctx.send(embed=self.embed)
+
+def setup(client):
+    client.add_cog(Standings(client))
+
+
+
