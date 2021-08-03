a99f61cadebae7522a782ce1713a5aa724aff2b3
Ryan Sowden <Ryan@Susans-MacBook-Pro.local>
2021-08-03 22:00:30 +1000

    Changed the way matchups are displayed

--- cogs/matchups.py
+++ cogs/matchups.py
@@ -33,9 +33,30 @@ class Matchups(commands.Cog):
             self.df = pd.DataFrame.from_dict(self.scoreboards)
             self.df[self.df.columns[[0,1,2,3]]]
             self.transpose_df = self.df.transpose()
-            self.table_data = tabulate(self.transpose_df, headers=['Team 1','Team 2'], showindex=False, tablefmt='plain')
-            self.table = str(re.sub(r"[),[('']", '', str(self.table_data)))
-            self.embed = discord.Embed(title=self.league_name['name'] + ' ' + 'Week' + ' ' + self.week + ' ' +  'Matchup', description=self.table, colour=5793266) #setting up the table to be embeded
+
+            '''
+            To make sure the matchups display correctly inside discord, each table result needs to be returned as an individual result and made into a seperate table
+            '''
+            self.table_data1 = "```" +  tabulate(self.df[1], headers=[], showindex=False, tablefmt='plain')+ "```"
+            self.table_data2 = "```" +  tabulate(self.df[2], headers=[], showindex=False, tablefmt='plain')+ "```"
+            self.table_data3 = "```" +  tabulate(self.df[3], headers=[], showindex=False, tablefmt='plain')+ "```"
+            self.table_data4 = "```" +  tabulate(self.df[4], headers=[], showindex=False, tablefmt='plain')+ "```"
+            self.table_data5 = "```" +  tabulate(self.df[5], headers=[], showindex=False, tablefmt='plain')+ "```"
+            self.table_data6 = "```" +  tabulate(self.df[6], headers=[], showindex=False, tablefmt='plain')+ "```"
+            self.table1 = str(re.sub(r"[),[('']", '', str(self.table_data1)))
+            self.table2 = str(re.sub(r"[),[('']", '', str(self.table_data2)))
+            self.table3 = str(re.sub(r"[),[('']", '', str(self.table_data3)))
+            self.table4 = str(re.sub(r"[),[('']", '', str(self.table_data4)))
+            self.table5 = str(re.sub(r"[),[('']", '', str(self.table_data5)))
+            self.table6 = str(re.sub(r"[),[('']", '', str(self.table_data6)))
+            #self.embed = discord.Embed(title=self.league_name['name'] + ' ' + 'Week' + ' ' + self.week + ' ' +  'Matchup', description=self.table, colour=5793266) #setting up the table to be embeded
+            self.embed = discord.Embed(title=self.league_name['name'] + ' ' + 'Matchups' + ' ' + 'Week' + ' ' + self.week, colour=5793266)
+            self.embed.add_field(name='1', value=self.table1, inline=False)
+            self.embed.add_field(name='2', value=self.table2, inline=False)
+            self.embed.add_field(name='3', value=self.table3, inline=False)
+            self.embed.add_field(name='4', value=self.table4, inline=False)
+            self.embed.add_field(name='5', value=self.table5, inline=False)
+            self.embed.add_field(name='6', value=self.table6, inline=False)
 
             await ctx.send(embed=self.embed)
 
