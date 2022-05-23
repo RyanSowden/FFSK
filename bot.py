2f2bd0c93670b8da9344090fcf81ee79fc4d53d1
Ryan Sowden <Ryan@Susans-MacBook-Pro.local>
2022-05-23 20:33:49 +1000

    Added help commnad

--- bot.py
+++ bot.py
@@ -11,52 +11,7 @@ GUILD = os.getenv('GUILD_ID')
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
-#tree = app_commands.CommandTree(client)
 
-
-'''
-@tree.command(guild=discord.Object(id=GUILD), name='ping',description='Test slash command')
-async def slash(interaction: discord.Interaction):
-    await interaction.response.send_message('Pong', ephemeral=True)
-
-@tree.command(guild=discord.Object(id=GUILD), name='test1',description='Test2 slash command')
-async def slash(interaction: discord.Interaction):
-    await interaction.response.send_message('Slash commands working correctly', ephemeral=True)
-@client.event
-async def on_ready():
-    synced = False #we use this so the bot doesn't sync commands more than once
-    await wait_until_ready()
-    if not synced: #check if slash commands have been synced 
-        await client.tree.copy_global_to(guild=discord.Object(id=GUILD)) 
-        synced = True
-    print("Commands successfully updated..")
-    print(f'{client.user} has connected to Discord!')
-
-#Error handling....
-@client.event
-async def on_command_error(ctx, error):
-    if isinstance(error, discord.ext.commands.errors.CommandNotFound):
-        await ctx.send("Sorry, that command wasn't found, enter ;help for a list of commands.")
-    elif isinstance(error, discord.ext.commands.MissingRequiredArgument):
-        await ctx.send("Please provied the required amount of arguments.")
-    elif isinstance(error, discord.ext.commands.MissingRole):
-        await ctx.send("You don't have the sufficient role to perform this action")
-    else:
-        raise error
-
-#error handling logging, *writes to err.log
-@client.event
-async def on_error(event, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
-        else:
-            raise
-
-@bot.command()
-async def load(ctx, extension):
-    bot.load_extension(f'cogs.{extension}')
-'''
 class Bot(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix='', intents=intents)
--- cogs/help.py
+++ cogs/help.py
@@ -1,29 +1,30 @@
 import discord 
+import os
+from discord import app_commands
 from discord.ext import commands
-from sleeper_wrapper import League
-import pandas as pd
 from tabulate import tabulate
-from db_connect import connection
-import re
 
 
+GUILD = os.getenv('GUILD_ID')
 
 class Help(commands.Cog):
 
-    def __init__(self,client):
-        self.client = client
+    def __init__(self, bot:commands.Bot) -> None:
+        self.bot = bot
 
 
-    @commands.command(name='help')
-    async def get_help(self,ctx):
-            self.embed = discord.Embed(title='Command List', colour=5793266)
-            self.embed.add_field(name=';assign', value='Assign a team name to a League ID,takes 2 arguments(league,#)(*must have sufficient role).', inline=False)
-            self.embed.add_field(name=';reassign', value='Reassign a team name ot a new League ID, takes 2 arguments(league,#)(*must have sufficient role).', inline=False)
-            self.embed.add_field(name=';lineups', value='Gets the current starting lineups for given league, takes 2 arguments(league,week)', inline=False)
-            self.embed.add_field(name=';matchups', value='Gets the current matchups for given league, takes 2 arguments(league,week)', inline=False)
-            self.embed.add_field(name=';standings', value='Gets the current standings for given league, takes 1 argument(league)', inline=False)
+    @app_commands.command(name='help')
+    async def get_help(self, interaction: discord.Interaction) -> None:
+        """Gets all available commands"""   
+        self.embed = discord.Embed(title='Command List', colour=5793266)
+        self.embed.add_field(name='assign', value='Assign a team name to a league id,takes 2 arguments(league,#)(*must have sufficient role).', inline=False)
+        self.embed.add_field(name='reassign', value='Reassign a team name ot a new League ID, takes 2 arguments(league,#)(*must have sufficient role).', inline=False)
+        self.embed.add_field(name='lineups', value='Gets the current starting lineups for given league, takes 2 arguments(league,week)', inline=False)
+        self.embed.add_field(name='matchups', value='Gets the current matchups for given league, takes 2 arguments(league,week)', inline=False)
+        self.embed.add_field(name='standings', value='Gets the current standings for given league, takes 1 argument(league)', inline=False)
 
-            await ctx.send(embed=self.embed)
+        await interaction.response.send_message(embed=self.embed)
 
-def setup(client):#setting up the disocrd client, must have this for COGS to work!
-    client.add_cog(Help(client))
+async def setup(bot: commands.Bot) -> None:
+  await bot.add_cog(Help(bot), guilds=[discord.Object(id=GUILD)])
+
