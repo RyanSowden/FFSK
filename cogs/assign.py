776d37717565f30465f58d82e0f90dbca814df60
Ryan Sowden <Ryan@Susans-MacBook-Pro.local>
2022-05-26 21:43:29 +1000

    Converted assign to slash command

--- cogs/assign.py
+++ cogs/assign.py
@@ -1,24 +1,24 @@
 import discord
 from discord.ext import commands
-from sleeper_wrapper import League
-import pandas as pd
-from tabulate import tabulate
+from discord import app_commands
+import os
 from db_connect import connection
 import re
             
 c = connection.cursor()
-
+GUILD = os.getenv('GUILD_ID')
 #function that allows user to assign leagues to names of their choice
 class Assign(commands.Cog):
 
     def __init___(self,client):
         self.client = client
 
-    @commands.command(name='assign', help='Assign league name to league ID')
+    @app_commands.command(name='assign')
     @commands.has_role("Admin")#Ensuring the approiate role is met, will be diffeerent for each server... must ensure user has the role to be able to use the below function
-    async def assign_league(self,ctx,arg1,arg2):
-        self.league_number = arg1
-        self.league_name = arg2
+    async def assign_league(self, interaction: discord.Interaction,league: str, number:str) -> None:
+        """Assign league name to league ID"""
+        self.league_number = number
+        self.league_name = league
         
         try:
             c.execute("SELECT * FROM league WHERE league_name = %s;",(self.league_name,))
@@ -27,16 +27,15 @@ class Assign(commands.Cog):
             if len(rows) == 0:
                 c.execute("INSERT INTO league (league_name,league_number) VALUES(%s,%s)",(self.league_name,self.league_number,))
                 connection.commit()
-                await ctx.send('League successfully assigned.')
+                await interaction.response.send_message('League successfully assigned.')
 
             else:
-                await ctx.send('League name or number already exists, please try again with a different combination.')
+                await interaction.response.send_message('League name or number already exists, please try again with a different combination.')
 
         except Exception:
                 connection.rollback()
-                await ctx.send('Whoops, something went wrong')
+                await interaction.response.send_message('Whoops, something went wrong')
 
-def setup(client): #setting up the disocrd client, must have this for COGS to work
-    client.add_cog(Assign(client))
-
+async def setup(bot: commands.Bot) -> None:
+  await bot.add_cog(Assign(bot), guilds=[discord.Object(id=GUILD)])
 
