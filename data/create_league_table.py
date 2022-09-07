3e37ceedb69d66903ca6fdc9dcad572106568d7a
Ryan Sowden <Ryan@Susans-MacBook-Pro.local>
2022-09-07 21:29:30 +1000

    Updated data type to varchar

--- data/create_league_table.py
+++ data/create_league_table.py
@@ -4,7 +4,7 @@ c = connection.cursor() #Establishing the connection to the database
 
 c.execute(
         '''CREATE TABLE IF NOT EXISTS league
-        (id SERIAL PRIMARY KEY, league_name text, league_number integer)'''
+        (id SERIAL PRIMARY KEY, league_name text, league_number varchar)'''
         )
 print('Table successfully created')
 connection.commit()# commiting the table to the database
